2a424b7e195e309824cea1d682eb3e9bcc5c11ba
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-03 18:21:53 +0300

    renderFunction

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ document.querySelector('#form-list').addEventListener('submit', (e) => {
     handlePost(newData)
 });
 
+function renderOnePhoto(photo){
+    let card = document.createElement('li');
+    card.className = 'card';
+    card.innerHTML = `
+    <div class="image-list">
+      <img src="${photo.image}" class="users">
+    </div>
+    <br>
+    <div class="content">
+        <h4>${photo.name}</h3>
+            <p>
+               $ <span class="donation-count">${photo.donation}</span> Donations
+            </p>
+            <br>
+            <p>${photo.description}</p>
+            <hr>
+    </div>
+    <div class="buttons">
+        <button id="donate">Donate $10</button>
+        <button id="set_free">Set free</button>
+    </div>
+      `;
+
+}
+
