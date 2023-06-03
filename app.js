24db5507a2cab3caf1c99d62026e3089828733e1
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-03 18:39:23 +0300

    async func for post dele...

--- app.js
+++ app.js
@@ -96,4 +96,16 @@ async function handleUpdate(newObj){
       }
 }
 
-async function handleDelete(id){}
+async function handleDelete(id){
+    try{
+        const response = await fetch(`http://localhost:3000/dataRender/${id}`, {
+            method: 'DELETE'
+        });
+        if (response.ok){
+            throw new Error('Failed to delete user.');
+        }
+    } catch (error) {
+        console.error(error);
+        alert(`An error occurred: ${error}`);
+      }
+}
